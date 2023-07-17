183daf76e2a93a7c16f1b45b5487ae200f1e9e0c
Tim Lawless <108081811+tlaw22@users.noreply.github.com>
2023-07-17 13:36:21 -0400

    Create index.js
    init

---
+++ index.js
@@ -0,0 +1,17 @@
+// Loops lesson practice
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+for (let i = 0; i < vacationSpots.length; i++){
+  console.log('I would love to visit ' + vacationSpots[i]);
+}
+
+
+// Loop in reverse
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter < 0; counter--){
+  console.log(counter);
+}
+
+// Nested Loops
+
