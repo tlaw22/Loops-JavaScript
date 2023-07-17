b13e46ddd6babb954034c2b6085039188ee32f1e
Tim Lawless <108081811+tlaw22@users.noreply.github.com>
2023-07-17 14:13:59 -0400

    Update index.js
    added Break lesson notes.

--- index.js
+++ index.js
@@ -43,4 +43,45 @@ do {
  console.log(cupsAdded + ' cup was added') 
 } while (cupsAdded < cupsOfSugarNeeded);
 
+// Using the break Kayword
+/*
+The break Keyword
+
+Imagine we’re looking to adopt a dog. We plan to go to the shelter every day for a year and then give up. But what if we meet our dream dog on day 65? We don’t want to keep going to the shelter for the next 300 days just because our original plan was to go for a whole year. In our code, when we want to stop a loop from continuing to execute even though the original stopping condition we wrote for our loop hasn’t been met, we can use the keyword break.
+
+The break keyword allows programs to “break” out of the loop from within the loop’s block.
+*/
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+for (let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.'){
+    break;
+  }
+}
+console.log("And if you don't know, now you know.");
+
+/*
+Review
+
+Great job! In this lesson, we learned how to write cleaner code with loops. You now know:
+
+    Loops perform repetitive actions so we don’t have to code that process manually every time.
+    How to write for loops with an iterator variable that increments or decrements
+    How to use a for loop to iterate through an array
+    A nested for loop is a loop inside another loop
+    while loops allow for different types of stopping conditions
+    Stopping conditions are crucial for avoiding infinite loops.
+    do...while loops run code at least once— only checking the stopping condition after the first execution
+    The break keyword allows programs to leave a loop during the execution of its block
+
+
+*/
+
+
+
+
+
+
+
 
